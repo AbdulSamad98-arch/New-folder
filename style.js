bb7091817918304ab2af286932f8d2c0d6da052b
AbdulSamad98-arch <abdulsamadcodes96@gmail.com>
2025-05-23 23:58:50 -0700

    Initial commit

---
+++ style.js
@@ -0,0 +1,8 @@
+
+// var age = prompt("enter your age")
+// if (age>18){alert("you are old enogh")}
+// else{alert("you are younger")}
+
+var num = prompt("what is 5+3?")
+if(5+3===8){alert("correct")}
+else{alert("not")}
